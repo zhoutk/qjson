2daec7e20eadf2ba57205ea52e402cb4230efa9a
zhoutk <zhoutk@189.cn>
2023-10-07 17:28:46 +0800

    array test .. number significant digit is 16

--- tests/test_array.cpp
+++ tests/test_array.cpp
@@ -13,6 +13,11 @@ private slots:
 	{
 		Json obj(JsonType::Array);
 		QVERIFY(obj.getValueType() == "Array");
+
+		obj.add(1).add(true).add(89.9023).add(9876543210123456)		//max number length
+			.add("This is a string value.").add(nullptr).add(Json({{"first","n01"}, {"second", "n02"}}));
+		QCOMPARE(obj.toString(), 
+			"[1,true,89.9023,9876543210123456,\"This is a string value.\",null,{\"first\":\"n01\",\"second\":\"n02\"}]");
 	}
 
 };
